46d3aa882d7bf23a935d0cc0bac4a247f804cc1d
Bänz Schenk <benz.schenk@brokkoli.be>
2022-03-30 19:33:59 +0200

    add an example

---
+++ example.php
@@ -0,0 +1,55 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use PonderSource\WSSE\{RSAOEAPEncryptionMethod, AES128GCMEncryptionMethod,C14NExcTransform, SHA256DigestMethod,SwAContentTransform,Security,BinarySecurityToken,EncryptedKey,EncryptionMethod,DigestMethod,MGF,KeyInfo,SecurityTokenReference,WSSecReference,CipherData,CipherValue,CipherReference,ReferenceList,DataReference,EncryptedData,Signature,SignatureMethod,SignedInfo,CanonicalizationMethod,DSigReference,C14NExcCanonicalization,RsaSha256SignatureMethod};
+use JMS\Serializer\SerializerBuilder;
+use phpseclib3\Crypt\{RSA, Random};
+use phpseclib3\File\X509;
+
+$clientPrivateKey = RSA::createKey();
+$clientPublicKey = $clientPrivateKey->getPublicKey();
+$issuer = new X509;
+$issuer->setPrivateKey($clientPrivateKey);
+$issuer->setDN([
+    'C' => 'Gondor',
+    'O' => 'Humanity',
+    'CN' => 'Denethor',
+    'L' => 'Minas Tirith'
+]);
+$subject = new X509;
+$subject->setPublicKey($clientPublicKey);
+$subject->setDN([
+    'C' => 'Gondor',
+    'O' => 'Humanity',
+    'CN' => 'Hirgon',
+    'L' => 'Minas Tirith'
+]);
+$x509 = new X509;
+$result = $x509->sign($issuer, $subject);
+$clientCertificate = $x509->loadX509($x509->saveX509($result));
+
+$serverPrivateKey = RSA::createKey();
+$serverCertificate = new X509;
+$serverCertificate->setPublicKey($serverPrivateKey->getPublicKey());
+$serverCertificate->setPrivateKey($serverPrivateKey);
+
+$encryptionKey = Random::string(32);
+
+$sha256 = new SHA256DigestMethod();
+$c14ne = new C14NExcTransform();
+
+$references = [
+    new DSigReference('#someid', '<some><content/></some>', [$c14ne], $sha256),
+    new DSigReference('#beacons', '<Arrow color="red"/>', [$c14ne], $sha256)
+];
+
+$sec = new Security();
+$sec->generateSignature($clientPrivateKey, $x509, $references, new C14NExcCanonicalization(), new RsaSha256SignatureMethod());
+$res = $sec->encryptData($encryptionKey, $x509, 'cid:whatever-man@cid', 'data is coming');
+
+$serializer = SerializerBuilder::create()->build();
+$xmlContent = $serializer->serialize($sec, 'xml');
+$dom = new DOMDocument();
+$dom->loadXml($xmlContent);
+var_dump($dom->C14N($exclusive=true));
